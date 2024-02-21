d59f0e0e05f5085e525770fd07b292158071e3cf
Vincent MONTÉCOT <vincent.montecot@hotmail.fr>
2024-02-21 16:24:13 +0100

    minor fix

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -6,15 +6,16 @@
 ;    By: vmonteco </var/spool/mail/vmonteco>        +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
 ;    Created: 2024/02/21 14:19:05 by vmonteco          #+#    #+#              ;
-;    Updated: 2024/02/21 16:17:01 by vmonteco         ###   ########.fr        ;
+;    Updated: 2024/02/21 16:24:05 by vmonteco         ###   ########.fr        ;
 ;                                                                              ;
 ;******************************************************************************;
 
 ft_strlen:
 	mov rax, 0
-
+	mov rcx, 0
+	
 _ft_strlen_loop:
-	cmp [rdi], 0
+	cmp [rdi], cl
 	jz _ft_strlen_end
 	inc rdi
 	inc rax
