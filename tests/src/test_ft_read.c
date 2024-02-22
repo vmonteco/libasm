35a909ad9dd804a19061b60214147237c7a6e36b
Vincent MONTÉCOT <vincent.montecot@hotmail.fr>
2024-02-22 15:40:59 +0100

    completed ft_read tests

---
+++ tests/src/test_ft_read.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_ft_read.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmonteco </var/spool/mail/vmonteco>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/22 10:02:50 by vmonteco          #+#    #+#             */
+/*   Updated: 2024/02/22 15:39:31 by vmonteco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tests.h"
+#define FILE_CONTENT = "Test with L1.\nTest with L2.\nTest with L3.\nTest with L4."
+
+void ft_read_testcase(int fd, size_t count)
+{
+	void		*buf;
+	ssize_t		chars_read;
+	
+	printf("## Test case - %d, 10\n");
+	if ((buf = malloc(BUFFER_SIZE + 1)))
+	{
+		if (fd = 0)
+		{
+			printf("Test with standard read, first line:\n", count);
+			chars_read = read(fd, buf, count);
+			printf("Characters read : %s.\nNumber of characters read : %d.\n", buf, chars_read);
+			printf("Test with standard read, second line:\n", count);
+			chars_read = read(fd, buf, count);
+			printf("Characters read : %s.\nNumber of characters read : %d.\n", buf, chars_read);
+			printf("Test with ft_read, first line :\n");
+			chars_read = read(fd, buf, count);
+			printf("Characters read : %s.\nNumber of characters read : %d.\n", buf, chars_read);
+			printf("Test with ft_read, second line :\n");
+			chars_read = read(fd, buf, count);
+			printf("Characters read : %s.\nNumber of characters read : %d.\n", buf, chars_read);
+		}
+	}
+	else
+	{
+		printf("Couldn't allocate");
+	}
+}
+
+void test_ft_read(void)
+{
+	printf("# ft_read test :\n");
+	ft_read_testcase(1, 0);
+	ft_read_testcase(1, 10);
+	ft_read_testcase(1, 100);
+	/* Create file */
+	fd = open("/tmp/libasm.test", 0_CREAT | O_WRITE);
+	write(fd, FILE_CONTENT, strlen(FILE_CONTENT));
+	ft_read_testcase(fd, 100);
+	close(fd);
+	remove("/tmp/libasm.test");
+}
