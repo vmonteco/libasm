62b160ba79bb939af46a22975aa4bf68761ea44e
Vincent MONTÉCOT <vincent.montecot@hotmail.fr>
2024-02-22 16:07:20 +0100

    corrected tes_ft_strdup.c

--- tests/src/test_ft_strdup.c
+++ tests/src/test_ft_strdup.c
@@ -6,10 +6,11 @@
 /*   By: vmonteco </var/spool/mail/vmonteco>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 15:49:27 by vmonteco          #+#    #+#             */
-/*   Updated: 2024/02/22 15:55:51 by vmonteco         ###   ########.fr       */
+/*   Updated: 2024/02/22 16:07:04 by vmonteco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include <string.h> 
 #include "tests.h"
 
@@ -17,12 +18,12 @@ void	ft_strdup_testcase(const char *s)
 {
 	char	*copy;
 
-	printf("## Test case - \"%s\", %x (address) :\n");
+	printf("## Test case - \"%s\", %lx (address) :\n", s, (unsigned long) s);
 	copy = strdup(s);
-	printf("Result with standard strdup : \"%s\" at %x.\n", copy, copy);
+	printf("Result with standard strdup : \"%s\" at %lx.\n", copy, (unsigned long) copy);
 	free(copy);
 	copy = strdup(s);
-	printf("Result with ft_strdup : \"%s\" at %x.\n", copy, copy);
+	printf("Result with ft_strdup : \"%s\" at %lx.\n", copy, (unsigned long) copy);
 	free(copy);
 }
 
