0214cc6495c0f83442131a1c724c596311573941
Vincent MONTÉCOT <vincent.montecot@hotmail.fr>
2024-02-22 06:16:14 +0100

    test added for ft_strcmp

--- includes/libasm.h
+++ includes/libasm.h
@@ -6,14 +6,18 @@
 /*   By: vmonteco </var/spool/mail/vmonteco>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 16:52:32 by vmonteco          #+#    #+#             */
-/*   Updated: 2024/02/22 02:16:36 by vmonteco         ###   ########.fr       */
+/*   Updated: 2024/02/22 06:03:43 by vmonteco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBASM_H
 # define LIBASM_H
 
-size_t	ft_strlen(const char *s);
-char	*ft_strcpy(char *restrict dst, const char *restrict src);
+size_t		ft_strlen(const char *s);
+char		*ft_strcpy(char *restrict dst, const char *restrict src);
+int			ft_strcmp(const char *s1, const char *s2);
+ssize_t		ft_write(int fd, const void *ptr, size_t count);
+ssize_t		ft_read(int fd, void *ptr, size_t count);
+char		*ft_strdup(const char *s);
 
 #endif
