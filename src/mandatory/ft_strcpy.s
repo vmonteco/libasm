09c64852b04d6a18d0c0294ee3447b3b69d4a037
Vincent MONTÉCOT <vincent.montecot@hotmail.fr>
2024-02-23 04:10:51 +0100

    ft_strcpy done

--- src/mandatory/ft_strcpy.s
+++ src/mandatory/ft_strcpy.s
@@ -6,25 +6,24 @@
 ;    By: vmonteco </var/spool/mail/vmonteco>        +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
 ;    Created: 2024/02/22 01:14:01 by vmonteco          #+#    #+#              ;
-;    Updated: 2024/02/22 17:29:38 by vmonteco         ###   ########.fr        ;
+;    Updated: 2024/02/23 04:10:22 by vmonteco         ###   ########.fr        ;
 ;                                                                              ;
 ;******************************************************************************;
 
 	global ft_strcpy
 
 ft_strcpy:
-	; rdi will contain dst
-	; rsi will contain src
-	; At the end, rax will contain the return value
 	push rdi
 	mov rax, 0
 	
 .loop:
 	cmp byte [rsi], 0
+	pushf
 	mov al, [rsi]
 	mov [rdi], al
 	inc rdi
 	inc rsi
+	popf
 	jz .end
 	jmp .loop
 	
