32ebbd0c5e1351cdc6cdbb2cd2b5a4e4abe9f37b
Vincent MONTÉCOT <vincent.montecot@hotmail.fr>
2024-02-26 22:21:31 +0100

    compiles, should still be corrected

--- src/mandatory/ft_strdup.s
+++ src/mandatory/ft_strdup.s
@@ -6,11 +6,26 @@
 ;    By: vmonteco </var/spool/mail/vmonteco>        +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
 ;    Created: 2024/02/23 04:49:50 by vmonteco          #+#    #+#              ;
-;    Updated: 2024/02/23 04:50:04 by vmonteco         ###   ########.fr        ;
+;    Updated: 2024/02/26 22:19:57 by vmonteco         ###   ########.fr        ;
 ;                                                                              ;
 ;******************************************************************************;
 
+	extern __errno_location
+	extern malloc
 	global ft_strdup
-
+	;; s in rdi
+	
 ft_strdup:
+	call malloc wrt ..plt
+	cmp rax, 0
+	jz .error_handling
+	jmp .end
+	
+.error_handling:
+	call	__errno_location wrt ..plt
+	mov		ebx, 0x0C
+	mov		[rax], ebx
+	mov		eax, 0
+	
+.end:
 	ret
